e54e22f663b91bd5d080e2d0399dbf9b86d5fda6
R0ckyRacoon <35631251+kurosasuke@users.noreply.github.com>
2022-10-27 22:46:37 +0200

    Update and rename main.py to Data_Cleaner.py

---
+++ Data_Cleaner.py
@@ -0,0 +1,64 @@
+import numpy as np
+import openpyxl
+import matplotlib.pyplot as plt
+
+# Get Excel data
+worksheet = openpyxl.load_workbook(filename=r"C:\Users\darii\OneDrive\Desktop\FC_data.xlsx")
+
+sheet = worksheet.active
+col_T = "C"
+col_V = "D"
+col_C = "E"
+
+Time = np.zeros(len(sheet[col_T]))
+Volt = np.zeros(len(sheet[col_V]))
+Curr = np.zeros(len(sheet[col_C]))
+
+# Import Seconds from Excel
+for i in range(len(sheet[col_T])):
+    if isinstance(sheet.cell(row=i + 1, column=3).value, int):
+        val_t = sheet.cell(row=i + 1, column=3).value
+        Time[i] = val_t
+
+# Import Voltage from Excel
+for i in range(len(sheet[col_V])):
+    if isinstance(sheet.cell(row=i + 1, column=4).value, float) or \
+            isinstance(sheet.cell(row=i + 1, column=4).value, int):
+        val_v = sheet.cell(row=i + 1, column=4).value
+        Volt[i] = val_v
+
+# Import Current from Excel
+for i in range(len(sheet[col_C])):
+    if isinstance(sheet.cell(row=i + 1, column=5).value, float) or \
+            isinstance(sheet.cell(row=i + 1, column=5).value, int):
+        val_c = sheet.cell(row=i + 1, column=5).value
+        Curr[i] = val_c
+
+# Ramping
+Ramps = np.eye(10800, dtype=int)
+# Row counter
+row = 0
+# Column counter
+counter = 0
+
+for i in range(len(Time)):
+    if Time[i] == 0 and Time[i+1] == 0:
+        counter += 1
+        Ramps[row, counter] = Time[i]
+        row += 1
+    else:
+        Ramps[row, counter] = Time[i]
+        row += 1
+
+# Plot results
+plt.scatter(Volt, Curr)
+
+# Trend-line
+z = np.polyfit(Volt, Curr, 7)
+p = np.poly1d(z)
+
+# Add trendline to scatter plot
+plt.plot(Volt, p(Volt), color="purple", linewidth=1, linestyle="--")
+
+# Show plot on Pycharm (maybe you doesn't need it)
+plt.show()

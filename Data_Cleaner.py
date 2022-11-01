ec281f416e29eb198ca018105edab5aeb9bceac5
R0ckyRacoon <35631251+kurosasuke@users.noreply.github.com>
2022-11-01 12:58:05 +0100

    Update Data_Cleaner.py

--- Data_Cleaner.py
+++ Data_Cleaner.py
@@ -10,58 +10,74 @@ col_T = "C"
 col_V = "D"
 col_C = "E"
 
-Time = np.zeros(len(sheet[col_T]))
-Volt = np.zeros(len(sheet[col_V]))
-Curr = np.zeros(len(sheet[col_C]))
+Time = []
+Volt = []
+Curr = []
 
 # Import Seconds from Excel
 for i in range(len(sheet[col_T])):
     if isinstance(sheet.cell(row=i + 1, column=3).value, int):
         val_t = sheet.cell(row=i + 1, column=3).value
-        Time[i] = val_t
+        Time.append(val_t)
 
 # Import Voltage from Excel
 for i in range(len(sheet[col_V])):
     if isinstance(sheet.cell(row=i + 1, column=4).value, float) or \
             isinstance(sheet.cell(row=i + 1, column=4).value, int):
         val_v = sheet.cell(row=i + 1, column=4).value
-        Volt[i] = val_v
+        Volt.append(val_v)
 
 # Import Current from Excel
 for i in range(len(sheet[col_C])):
     if isinstance(sheet.cell(row=i + 1, column=5).value, float) or \
             isinstance(sheet.cell(row=i + 1, column=5).value, int):
         val_c = sheet.cell(row=i + 1, column=5).value
-        Curr[i] = val_c
+        Curr.append(val_c)
 
 # Time start from zero
-Time = Time[252:]
-Volt = Volt[252:]
-Curr = Curr[252:]
+Time = Time[250:]
+Volt = Volt[250:]
+Curr = Curr[250:]
 
 # Clean data from zeros
-Volt_n = np.zeros(len(Volt))
-Curr_n = np.zeros(len(Curr))
+Volt_n = []
+Curr_n = []
 
 for i in range(len(Volt)):
     if Volt[i] != 0:
-        Volt_n[i] = Volt[i]
-        Curr_n[i] = Curr[i]
+        Volt_n.append(Volt[i])
+        Curr_n.append(Curr[i])
 
-# Ramping
+# Ramping variables
+new_Time = []
+fake_time = []
+
+# Distance between two ramping sets
 for i in range(len(Time)):
-    if Time[i] == 0 and Time[i + 1] == 0:
-        new_Time = np.array_split(Time, 66)
+    if Time[i - 1] != 0 and Time[i] == 0 and Time[i + 1] == 0:
+        new_Time.append(fake_time)
+        fake_time = []
+    else:
+        fake_time.append(Time[i])
+
+# Delete I<40A
+new_Volt = []
+new_Curr = []
+
+for i in range(len(Volt_n)):
+    if Volt_n[i] > 40:
+        new_Volt.append(Volt_n[i])
+        new_Curr.append(Curr_n[i])
 
 # Plot results
-plt.scatter(Volt_n, Curr_n)
+plt.scatter(new_Volt, new_Curr)
 
 # Trend-line
-z = np.polyfit(Volt_n, Curr_n, 7)
+z = np.polyfit(new_Volt, new_Curr, 7)
 p = np.poly1d(z)
 
 # Add trend-line to scatter plot
-plt.plot(Volt_n, p(Volt_n), color="purple", linewidth=1, linestyle="--")
+plt.plot(new_Volt, p(new_Volt), color="purple", linewidth=1, linestyle="--")
 
 # Show plot on Pycharm (maybe you doesn't need it)
 plt.show()
